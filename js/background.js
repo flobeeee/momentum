819a30372b8c29b89bbc3c3a70ac70cf64b6f05d
flobeeee <sssver28@gmail.com>
2021-12-08 00:02:20 +0900

    크리스마스 테마 적용

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = [
+/*const images = [
   "0.jpg",
   "1.jpg",
   "2.jpg"
@@ -12,3 +12,26 @@ document.body.style.backgroundImage = `url(img/${chosenImage})`;
 document.body.style.backgroundSize = 'cover'
 
 // document.body.appendChild(bgImage)
+*/
+
+// 별찍기
+
+function handleMakeStars(event) {
+  const span = document.createElement("span")
+  const randomSize = Math.floor(Math.random() * 30)
+  // span.innerHTML = "⭐️"
+  span.style.backgroundColor = "yellow"
+  span.style.position = "fixed"
+  span.style.left = `${event.clientX}px`
+  span.style.top = `${event.clientY}px`
+  span.style.height = `${randomSize}px`
+  span.style.width = `${randomSize}px`
+  span.style.borderRadius = '30px'
+  setInterval(() => {
+    span.style.backgroundColor = 'rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+    span.style.display === 'none' ? span.style.display = 'inline-block' : span.style.display = 'none';
+  }, Math.floor(Math.random() * 10000) + 1000);
+  document.body.appendChild(span)
+}
+
+window.addEventListener("click", handleMakeStars)
